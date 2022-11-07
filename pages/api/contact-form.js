65478cb9ffd99eec662d44806b5fd5575723c488
conor.ney <conor.ux@gmail.com>
2022-11-07 00:34:22 -0500

    contact form working

--- pages/api/contact-form.js
+++ pages/api/contact-form.js
@@ -1,9 +1,26 @@
-export default function (req, res) {
-  console.log(req.body);
-  let nodemailer = require('nodemailer')
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: s
-  })
-  
+export default async function mainMail(req, res) {
+  const { email, name, message } = req.body;
+
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SECRETCHEF_SG);
+  const msg = {
+    to: process.env.EMAIL,
+    from: process.env.GMAIL_USER,
+    subject: 'New Message From Contact Form',
+    html: `You received a message from:
+    Name: ${name}
+    Email: ${email}
+    Message: ${message}`,
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('email sent');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.status(200);
 }
